c9a14b91960cee1339b8983aab5d892b67352ce8
scraft1 <spencercraft@byu.edu>
2015-01-13 21:44:51 -0700

    Update UserData.sh

--- UserData.sh
+++ UserData.sh
@@ -2,7 +2,6 @@
 yum update -y
 yum groupinstall -y "Web Server" "MySQL Database" "PHP Support"
 yum install -y php-mysql
-yum install git -y
 service httpd start
 chkconfig httpd on
 groupadd www
@@ -12,8 +11,9 @@ chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} +
 find /var/www -type f -exec chmod 0664 {} +
 
+yum install -y git
 git clone https://github.com/scraft1/462.git
-cp 462/index.html /var/www/html
+cp 462/index.html > /var/www/html
 
 service mysqld start
 mysql_secure_installation
